af8ae70cee2e45714f69bf0399c136acd60722da
Tomkiv_Ihor <igortomkiw3@gmail.com>
2026-03-08 18:40:49 +0200

    feat: add TMDBResponse interface for type safety in movie fetching

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -2,6 +2,10 @@ import { type Movie } from "../types/movie";
 import axios from "axios";
 const key = import.meta.env.VITE_TMDB_TOKEN;
 
+interface TMDBResponse {
+  results: Movie[];
+}
+
 const api = axios.create({
   //створення Екземлпяра тому що чи часто використовуємо один і тоай самий токен !
   baseURL: "https://api.themoviedb.org/3/search/movie",
@@ -9,7 +13,7 @@ const api = axios.create({
 });
 
 export const fetchMovies = async (nameMovie: string): Promise<Movie[]> => {
-  const response = await api.get("", {
+  const response = await api.get<TMDBResponse>("", {
     params: {
       query: nameMovie, //обовязоковий параметр для нашого запиту
     },
